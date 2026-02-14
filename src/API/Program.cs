df0459578a5bf45b22e8c596f7ce6afbeb8bba59
theshameem <theshameem.alam@gmail.com>
2026-02-14 10:55:47 +0600

    fix: open swagger on run the project default

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -29,7 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
+        c.RoutePrefix = string.Empty;
+	});
 }
 
 app.UseHttpsRedirection();
